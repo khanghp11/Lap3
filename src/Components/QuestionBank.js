0d2e0c05900f043b9f18a5246253b1cef948d77d
khanghp11 <khanghuynh.ornn@gmail.com>
2024-06-08 20:57:47 +0700

    lap3

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the largest mammal in the world?",
+        options: ["African Elephant", "Orca", "Giraffe", "Blue Whale"],
+        answer: "Blue Whale",
+    },
+    {
+        id: 2,
+        question: "Which bird is known for its impressive ability to mimic sounds, including human speech?",
+        options: ["Crow", "Eagle", "Pigeon", "Parrot"],
+        answer: "Parrot",
+    },
+    {
+        id: 3,
+        question: "Which animal is known as the King of the Jungle?",
+        options: ["Tiger", "Lion", "Bear", "Leopard"],
+        answer: "Lion"
+    },
+    {
+        id: 4,
+        question: "What is the fastest land animal?",
+        options: ["Cheetah", "Falcon", "Horse", "Antelope"],
+        answer: "Cheetah"
+    },
+    {
+        id: 5,
+        question: "Which animal has the longest lifespan?",
+        options: ["Elephant", "Blue Whale", "Galápagos Tortoise", "Human"],
+        answer: "Galápagos Tortoise"
+    },
+]
+ 
+export default qBank;
